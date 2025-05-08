d2cba5c90f132e9005b3ece487bc65950919744e
VitaliyKorzuhin <k9519511071@gmail.com>
2025-05-08 20:07:58 +0500

    add small coment. i need to think

--- main.py
+++ main.py
@@ -6,6 +6,6 @@ def filter_by_state(list_of_dictionaries: List[Dict[str, str]], state: str = 'EX
     return [item for item in list_of_dictionaries if item.get('state') == state]
 
 def sort_by_date(list_of_dictionaries: List[Dict[str, str]], reverse: bool = True) -> List[Dict[str, str]]:
-    """ Функция сортировки по дате."""
+    """ Функция сортировки по дате может изменить reverse на True по умолчанию"""
 
     return sorted(list_of_dictionaries, key=lambda item: item.get('date'), reverse=reverse)
