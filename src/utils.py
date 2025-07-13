43d97f47caf9350a02ccfede65278d9569890936
Vitaliy Korzuhin <147917921+Vitaliy951@users.noreply.github.com>
2025-07-13 18:28:53 +0500

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -8,19 +8,21 @@ from typing import List, Dict, Union
 logger = logging.getLogger('utils')
 logger.setLevel(logging.DEBUG)
 
-""" Создание обработчика для записи логов в файл """
+""" Создание директории для логов, если она не существует """
+log_directory = 'logs'
+if not os.path.exists(log_directory):
+    os.makedirs(log_directory)
 
-file_handler = logging.FileHandler('logs/utils.log')
+""" Создание обработчика для записи логов в файл """
+file_handler = logging.FileHandler(os.path.join(log_directory, 'utils.log'))
 file_handler.setLevel(logging.DEBUG)
 
 """ Настройка форматирования логов """
-
 file_formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
 file_handler.setFormatter(file_formatter)
 
 """ Добавление обработчика к логгеру """
 logger.addHandler(file_handler)
-
 
 def read_json_file(file_path: str) -> Union[List[Dict], None]:
     """Читает JSON-файл и возвращает список словарей с данными о транзакциях или None в случае ошибки."""
